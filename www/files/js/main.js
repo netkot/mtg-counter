44ab294cfa8e77d008032fe5d3ebb55fee041935
Netkot N <netkot@gmail.com>
2023-11-17 19:57:23 +0200

    Release

--- www/files/js/main.js
+++ www/files/js/main.js
@@ -1,6 +1,7 @@
 $(function() 
     {
     click_snd = new Audio("files/sounds/click.mp3");
+    dices_snd = new Audio("files/sounds/dice_roll.mp3");
     
     init_2_player_layout ();
 //    init_3_player_layout ();
@@ -49,6 +50,17 @@ function init_events ()
         reset_counters ();
         toggleSettings();
         });
+    $('.settings .dice').click(function()
+        {
+        play_click_snd ();
+        toggleSettings();
+        roll_dices ();
+
+        // Fade out dices
+        setTimeout(function() {
+            $('.player .dice').fadeOut()
+            }, 5000);
+        });
     $('.settings .two_player').click(function()
         {
         play_click_snd ();
@@ -59,6 +71,12 @@ function init_events ()
         {
         play_click_snd ();
         init_3_player_layout ();
+        toggleSettings();
+        });
+    $('.settings .four_player').click(function()
+        {
+        play_click_snd ();
+        init_4_player_layout ();
         toggleSettings();
         });
     }
@@ -87,8 +105,8 @@ function set_color(obj)
 function toggleSettings()
     {
     $('.settings').toggleClass('active');
+    $('.fullscreen').toggleClass('active');
     $('.mana').toggleClass('active');
-    $('.fullscreen').toggle();
     }
 function reset_counters ()
     {
@@ -101,9 +119,117 @@ function play_click_snd ()
     click_snd.currentTime=0;
     }
 
+function roll_dices ()
+    {
+    $('.player .dice').show();
+
+    dices_snd.play(); 
+    dices_snd.currentTime=0;
+
+    how_many_rolls = 10;
+    roll_cnt = 0;
+    slow_roll_dices ();
+    }
+
+
+function slow_roll_dices ()
+    {
+    max_roll = 0;
+    max_roll_cnt = 1;
+    last_val = 0;
+    $('.player .dice').each(function(index) {
+        val =  randomIntFromInterval(1, 6);
+        if (val > max_roll)
+            max_roll = val;
+        else    
+            {
+            if (max_roll == val)
+                max_roll_cnt++;
+            }
+
+        class_name = 'd' + val;
+        $(this).removeClass().addClass('dice rolled ' + class_name);
+        last_val = val;
+//        console.log ('p' + index + ':' + val);
+        });
+    roll_cnt++;
+
+//    console.log ('=================================');
+
+    if (roll_cnt < how_many_rolls)
+        {
+        setTimeout(function() {
+            slow_roll_dices ();
+            }, roll_cnt * 15);
+        }
+    else
+        {
+//        console.log ('max_roll     = ' + max_roll);
+//        console.log ('max_roll_cnt = ' + max_roll_cnt);
+
+        if (max_roll_cnt > 1)
+            {
+//            console.log ('Fast reroll!');
+            fast_roll_dices ();
+            }
+
+        setTimeout(function() {
+            stop_roll ();
+            }, 100);
+        }
+    }
+
+
+function fast_roll_dices ()
+    {
+    max_roll = 0;
+    max_roll_cnt = 1;
+    last_val = 0;
+    $('.player .dice').each(function(index) {
+        val =  randomIntFromInterval(1, 6);
+        if (val > max_roll)
+            max_roll = val;
+        else    
+            {
+            if (max_roll == val)
+                max_roll_cnt++;
+            }
+        class_name = 'd' + val;
+        $(this).removeClass().addClass('dice static ' + class_name);
+
+        last_val = val;
+//        console.log ('p' + index + ':' + val);
+        });
+    roll_cnt++;
+
+//    console.log ('=================================');
+
+
+    if (max_roll_cnt > 1)
+        {
+        fast_roll_dices ();
+//        console.log ('Fast reroll! Again!');
+        }
+
+    }
+
+function stop_roll ()
+    {
+    $('.player .dice').removeClass('rolled').addClass('static');
+    }
+
+function randomIntFromInterval(min, max) 
+    {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+
+
 function init_2_player_layout ()
     {
     $( ".clone").remove();
+    $( ".og" ).removeClass('r90');
+
     $( ".main_wrap" ).removeClass('layout_3_playes layout_4_playes').addClass('layout_2_playes');
     $( ".og" ).clone().appendTo( ".main_wrap" ).addClass('clone p2').removeClass('og').css('background','#000').css('color','#fff');
     reset_counters ();
@@ -111,6 +237,7 @@ function init_2_player_layout ()
 function init_3_player_layout ()
     {
     $( ".clone").remove();
+    $( ".og" ).removeClass('r90');
     $( ".main_wrap" ).removeClass('layout_2_playes layout_4_playes').addClass('layout_3_playes');
     $( ".og" ).clone().appendTo( ".main_wrap" ).addClass('clone p2 r90').removeClass('og').css('background','#000').css('color','#fff');
     $( ".og" ).clone().appendTo( ".main_wrap" ).addClass('clone p2 r-90').removeClass('og').css('background','#f85555');
@@ -119,6 +246,11 @@ function init_3_player_layout ()
 function init_4_player_layout ()
     {
     $( ".clone").remove();
-    $( ".main_wrap" ).removeClass('layout_2_playes layout_3_playes').addClass('layout_4_playes');
+    init_3_player_layout ();
+    $( ".main_wrap" ).removeClass('layout_3_playes layout_2_playes').addClass('layout_4_playes');
+
+    $( ".og" ).addClass('r90');
+    $( ".og" ).clone().appendTo( ".main_wrap" ).addClass('clone p4 r-90').removeClass('og').css('background','#67c1f5');
+
     reset_counters ();
     }
